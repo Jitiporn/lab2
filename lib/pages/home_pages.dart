6c61d793daa5f096b1959bc6f46fa9d1266aa14a
testjitiporn043 <652021043@tsu.ac.th>
2024-11-21 17:22:27 +0700

    first commit

---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 233, 194, 207),
+      appBar: AppBar(
+        title: Text("652021043"),
+        backgroundColor: const Color.fromARGB(255, 192, 82, 119),
+      ),
+    );
+  }
+}
